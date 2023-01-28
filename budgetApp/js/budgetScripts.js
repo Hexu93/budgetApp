fa34ca62e2ab95781e89b3a8f14198a9e92bf51e
Hexu93 <95844460+Hexu93@users.noreply.github.com>
2023-01-28 17:39:28 +0200

    Done stuff, now it works

--- budgetApp/js/budgetScripts.js
+++ budgetApp/js/budgetScripts.js
@@ -1,4 +1,4 @@
-class UI 
+class UI //Luokan kanssa on helpompi työskennellä
 {
     constructor() 
     {
@@ -17,21 +17,216 @@ class UI
       this.itemList = [];
       this.itemID = 0;
     }
+
+    //Submit budget
+    submitBudgetForm()
+    {
+        const value = this.budgetInput.value;
+        if(value === '' || value < 0)
+        {
+            this.budgetFeedback.classList.add('showItem');
+            this.budgetFeedback.innerHTML = `<p>Value cannot be empty or neggative </p>`;
+
+            //This lets the timeout timer to use 'this' argument from the constructor
+            const self = this;
+            setTimeout(function()
+            {
+                self.budgetFeedback.classList.remove('showItem');
+            }, 4000);
+        }
+        else
+        {
+            this.budgetAmount.textContent = value;
+            this.budgetInput.value = '';
+            this.showBalance();
+        }
+    }
+
+    //Show balance
+    showBalance()
+    {
+        const expense = this.totalExpense();
+        const total = parseInt(this.budgetAmount.textContent) - expense;
+        this.balanceAmount.textContent = total;
+        if(total < 0)
+        {
+            this.balance.classList.remove('showGreen', 'showBlack');
+            this.balance.classList.add('showRed');
+        }
+
+        else if (total > 0)
+        {
+            this.balance.classList.remove('showRed', 'showBlack');
+            this.balance.classList.add('showGreen');
+        }
+
+        else if (total === 0)
+        {
+            this.balance.classList.remove('showRed', 'showGreen');
+            this.balance.classList.add('showBlack');
+        }
+    }
+
+    //Submit expenses
+    submitExpenseForm()
+    {
+        const expenseValue = this.expenseInput.value;
+        const amountValue  = this.amountInput.value;
+
+        if (expenseValue === '' || amountValue === '' || amountValue < 0)
+        {
+            this.expenseFeedback.classList.add('showItem');
+            this.expenseFeedback.innerHTML = `<p>Values cannot be empty or negative</p>`;
+            const self = this;
+            setTimeout(function(){
+                self.expenseFeedback.classList.remove('showItem');
+            },4000);
+        }
+
+        else
+        {
+            let amount = parseInt(amountValue);
+            this.expenseInput.value = '';
+            this.amountInput.value = '';
+
+            let expense = {
+                id: this.itemID,
+                title: this.expenseValue,
+                amount,
+
+            }
+            this.itemID++;
+            this.itemList.push(expense);
+
+            this.addExpense(expense);
+            this.showBalance();
+            
+        }
+    }
+
+    //Add expense
+    addExpense(expense)
+    {
+        const div = document.createElement('div');
+        div.classList.add('expense');
+        div.innerHTML = `
+        <div class="expense-item d-flex justify-content-between align-items-baseline">
+
+        <h6 class="expense-title mb-0 text-uppercase list-item">${expense.title}</h6>
+        <h5 class="expense-amount mb-0 list-item">${expense.amount}</h5>
+
+        <div class="expense-icons list-item">
+
+         <a href="#" class="edit-icon mx-2" data-id="${expense.id}">
+          <i class="fas fa-edit"></i>
+         </a>
+         <a href="#" class="delete-icon" data-id="${expense.id}">
+          <i class="fas fa-trash"></i>
+         </a>
+        </div>
+       </div>`;
+
+       this.expenseList.appendChild(div);
+    }
+
+
+    //Total expense
+    totalExpense()
+    {
+        let total = 0;
+
+        if(this.itemList.length > 0)
+        {
+            total = this.itemList.reduce(function(acc,curr)
+            {
+                //Accumulator and current value
+                acc += curr.amount;
+                return acc;
+            },0)
+        }
+        this.expenseAmount.textContent = total;
+        return total;
+    }
+
+    editExpense(element)
+    {
+        let id = parseInt(element.dataset.id);
+        let parent = element.parentElement.parentElement.parentElement;
+
+        //Remove from DOM
+        this.expenseList.removeChild(parent);
+
+        let expense = this.itemList.filter(function(item)
+        {
+            return item.id === id;
+        });
+
+        this.expenseInput.value = expense[0].title;
+        this.amountInput.value = expense[0].amount;
+
+        let templist = this.itemList.filter(function(item)
+        {
+            return item.id !== id;
+        });
+
+        this.itemList = templist;
+        this.showBalance();
+    }
+
+    deleteExpense(element)
+    {
+        let id = parseInt(element.dataset.id);
+        let parent = element.parentElement.parentElement.parentElement;
+
+        //Remove from DOM
+        this.expenseList.removeChild(parent);
+
+        let templist = this.itemList.filter(function(item)
+        {
+            return item.id !== id;
+        });
+
+        this.itemList = templist;
+        this.showBalance();
+
+    }
 }
 
 function eventListners()
 {
     const budgetForm = document.getElementById('budget-form');
-    const expenseForm = document.getElementById("expense-form");
-    const expenseList = document.getElementById("expense-list");
+    const expenseForm = document.getElementById('expense-form');
+    const expenseList = document.getElementById('expense-list');
 
-    const ui = new UI()
+    //Päivitetään UI
+    const ui = new UI();
     
     budgetForm.addEventListener('submit', function(event)
     {
-        
-    })
+       event.preventDefault();
+       ui.submitBudgetForm();
+    });
     
+    //submit list
+    expenseForm.addEventListener('submit', function(event)
+    {
+        event.preventDefault()
+        ui.submitExpenseForm();
+    });
+
+    //click list
+    expenseList.addEventListener('click', function(event)
+    {
+        if(event.target.parentElement.classList.contains('edit-icon'))
+        {
+            ui.editExpense(event.target.parentElement);
+        }
+        else if(event.target.parentElement.classList.contains('delete-icon'))
+        {
+            ui.deleteExpense(event.target.parentElement);   
+        }
+    });
+
 }
 
 document.addEventListener('DOMContentLoaded', function()
